79cd121512d844be4502bd964e1af428f1504b36
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2023-03-07 11:19:43 -0500

    Testing getAccessToken

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -60,6 +60,7 @@ module.exports.getAccessToken = async (event) => {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify(token),
     };
