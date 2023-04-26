67d83fcb9ed2d45d2d3d59019029ec2e2d863a98
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2023-04-26 11:01:25 -0400

    Server changes

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -5,12 +5,16 @@ import { mockData } from '../mock-data';
 import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
-  let locations, CitySearchWrapper;
+  let locations;
+  let CitySearchWrapper;
   beforeAll(() => {
     locations = extractLocations(mockData);
     CitySearchWrapper = shallow(<CitySearch locations={locations} />);
   });
-});
+
+    test("render text input", () => {
+      expect(CitySearchWrapper.find(".city")).toHaveLength(1);
+  });
 
     test('renders a list of suggestions', () => {
       expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
@@ -52,3 +56,12 @@ describe('<CitySearch /> component', () => {
     });
     expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
   });
+
+  test("selecting a suggestion should change query state", () => {
+    CitySearchWrapper.setState({
+      query: 'Berlin'  });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+    expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+  });
+});
